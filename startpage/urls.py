9a809d900132888d25dc03da7f13b9685f9c1213
Christopher Trevino <christophertrev@gmail.com>
2014-05-04 21:02:22 -0700

    Added Coverpage, uploading images

---
+++ startpage/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import patterns, include, url
+
+from startpage import views
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+
+    url(r'^$', views.cover, name='cover'),
+
+
+
+)
--- startpage/views.py
+++ startpage/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
+
+
+def index(request):
+
+
+	return render(request, 'startpage/index.html')
+    #return HttpResponse("Rango says hello world!")
+
+
+def cover(request):
+
+
+	return render(request, 'startpage/cover.html')
 
 # Create your views here.
