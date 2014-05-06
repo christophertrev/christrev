76ef14071e972dadf97963710406b36c6f7b5955
Christopher Trevino <christophertrev@gmail.com>
2014-05-05 22:39:45 -0700

    removed app. we dont need it. Simple webpage

--- christrev/urls.py
+++ christrev/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls import patterns, include, url
-
+import views
 from django.contrib import admin
 admin.autodiscover()
 
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', include('startpage.urls')),
+    #url(r'^$', include('startpage.urls', namespace='startpage', app_name='startpage')),
+    url(r'^about/', views.index, name='about'),
 
+    url(r'^$', views.cover, name='cover'),
 
 )
